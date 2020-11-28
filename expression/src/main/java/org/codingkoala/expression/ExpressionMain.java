67e3a20aa3fa3982d94db322651956c2fd8f0f42
CodingKoala-1991 <alancjchen1991@gmail.com>
2020-11-28 22:02:38 +0800

    表达式可以初步运行

---
+++ expression/src/main/java/org/codingkoala/expression/ExpressionMain.java
@@ -0,0 +1,26 @@
+package org.codingkoala.expression;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ExpressionMain {
+    public static void main(String[] args) throws IOException {
+        // 如果是本地 idea 直接运行，就用这种方式获取 resources 目录的文件
+        // 打成 jar 包去运行也可以直接执行命令 "java -jar ./target/expression-1.0-SNAPSHOT-jar-with-dependencies.jar"
+        InputStream inputStream = ExpressionMain.class.getClassLoader().getResourceAsStream("expressionQueries");
+        assert inputStream != null;
+        ANTLRInputStream input = new ANTLRInputStream(inputStream);
+
+        LabeledExprLexer lexer = new LabeledExprLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LabeledExprParser parser = new LabeledExprParser(tokens);
+        ParseTree tree = parser.prog();
+
+        EvalVisitor visitor = new EvalVisitor();
+        visitor.visit(tree);
+    }
+}
